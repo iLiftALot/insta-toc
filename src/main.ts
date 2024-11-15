844ca4efa10efd860a827477251841e1ef4b2bc0
iLiftALot <nickcorbin17@yahoo.com>
2024-11-15 17:40:46 -0500

    Created dedicated class for managing the TOC

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ import { InstaTocSettings, DEFAULT_SETTINGS } from './Settings';
 import { SettingTab } from './SettingsTab';
 import { deepmerge } from 'deepmerge-ts';
 import { debounce } from 'Utility';
+import { ManageToc } from './ManageToc';
 
 
 export default class InstaTocPlugin extends Plugin {
@@ -88,61 +89,6 @@ export default class InstaTocPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	// Provides the insert location range for the new insta-toc codeblock
-	private getTocInsertPosition(instaTocSection: SectionCache): EditorRange {
-		// Define the star/end line/character index
-		const startLine: number = instaTocSection.position.start.line;
-		const startCh = 0;
-		const endLine: number = instaTocSection.position.end.line;
-		const endCh: number = instaTocSection.position.end.col;
-
-		const tocStartPos: EditorPosition = { line: startLine, ch: startCh };
-		const tocEndPos: EditorPosition = { line: endLine, ch: endCh };
-
-		return { from: tocStartPos, to: tocEndPos };
-	}
-
-	// Generates a new insta-toc codeblock
-	private generateToc(fileHeadings: HeadingCache[]): string {
-		const tocHeadingRefs: string[] = [];
-
-		// Iterate each heading cache object to generate the new TOC content
-		if (fileHeadings.length > 0) {
-			fileHeadings.forEach((headingCache: HeadingCache) => {
-				const headingLevel: number = headingCache.level;
-				const headingText: string = headingCache.heading;
-				const tocHeadingRef = `${' '.repeat((headingLevel - 1) * 4)}- ${headingText}`;
-
-				tocHeadingRefs.push(tocHeadingRef);
-			});
-		}
-
-		const tocContent: string = tocHeadingRefs.join('\n');
-		return `\`\`\`insta-toc\n${tocContent}\n\`\`\``;
-	}
-
-	// Dynamically update the TOC
-	private updateAutoToc(editor: Editor, file: TFile): void {
-		// Extract the headings and sections from the active file's cache
-		const fileCache: CachedMetadata | null = this.app.metadataCache.getFileCache(file);
-		const fileHeadings: HeadingCache[] = fileCache?.headings ?? [];
-		const instaTocSection: SectionCache | undefined = fileCache?.sections
-			? fileCache.sections.find(
-				(section: SectionCache) => section.type === 'code' &&
-					editor.getLine(section.position.start.line) === '```insta-toc'
-			) : undefined;
-
-		// Return early if no sections (which means no insta-toc blocks)
-		if (!instaTocSection) return;
-
-		// Get the insertion position and generate the updated TOC
-		const tocInsertRange: EditorRange = this.getTocInsertPosition(instaTocSection);
-		const newTocBlock = this.generateToc(fileHeadings);
-
-		// Replace the old TOC with the updated TOC
-		editor.replaceRange(newTocBlock, tocInsertRange.from, tocInsertRange.to);
-	}
-
 	// Main control method to handle all active file changes
 	private async handleEditorChange(): Promise<void> {
 		const activeEditor: MarkdownFileInfo | null = this.app.workspace.activeEditor;
@@ -152,6 +98,6 @@ export default class InstaTocPlugin extends Plugin {
 		if (!activeEditor || !editor || !file) return;
 
 		// Dynamically update the insta-toc codeblock
-		this.updateAutoToc(editor, file);
+		new ManageToc(this.app, this, editor, file);
 	}
 }
