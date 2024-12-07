b838dedc347103e10c883c2fb1c70ba22a596a85
iLiftALot <nickcorbin17@yahoo.com>
2024-12-07 09:20:42 -0500

    - Added `excludedHeadingLevels` to present an option to globally exclude specific heading levels
    - Added `excludedHeadingText` to present an option to globally exclude specific headings entirely

--- src/SettingsTab.ts
+++ src/SettingsTab.ts
@@ -8,8 +8,8 @@ import {
     TextComponent
 } from 'obsidian';
 import InstaToc from './main';
-import { BulletTypes } from './constants';
-import { BulletType, IndentLevel, UpdateDelay } from "./types";
+import { BulletTypes, DefaultExcludedChars } from './constants';
+import { BulletType, HeadingLevel, IndentLevel, UpdateDelay } from "./types";
 
 
 export class SettingTab extends PluginSettingTab {
@@ -31,6 +31,7 @@ export class SettingTab extends PluginSettingTab {
         tabTitle.nameEl.classList.add('setting-title');
         tabTitle.controlEl.remove();
 
+        // Global list bullet style
         new Setting(containerEl)
             .setName('List bullet style')
             .setDesc('Select the global list bullet type.')
@@ -44,6 +45,7 @@ export class SettingTab extends PluginSettingTab {
                     })
             );
 
+        // Global indentation width
         new Setting(containerEl)
             .setName('Indentation width')
             .setDesc('Select the global indentation size.')
@@ -59,9 +61,10 @@ export class SettingTab extends PluginSettingTab {
                     })
             );
 
+        // Update delay
         new Setting(containerEl)
             .setName('Update delay')
-            .setDesc('The delay for each TOC update.')
+            .setDesc('The delay for each ToC update.')
             .addSlider((component: SliderComponent) => {
                 component
                     .setLimits(500, 10000, 500)
@@ -72,12 +75,13 @@ export class SettingTab extends PluginSettingTab {
                         this.plugin.settings.updateDelay = value;
                         await this.plugin.saveSettings();
                         this.plugin.updateModifyEventListener();
-                    })
+                    });
             });
 
+        // Global ToC title
         new Setting(containerEl)
             .setName('ToC Title')
-            .setDesc('The global title of the generated Table of Contents.')
+            .setDesc('The global title for the Table of Contents.')
             .addText((component: TextComponent) => {
                 component
                     .setValue(this.plugin.settings.tocTitle)
@@ -85,22 +89,75 @@ export class SettingTab extends PluginSettingTab {
                         this.plugin.settings.tocTitle = value;
                         await this.plugin.saveSettings();
                     });
-            });
+                
+                component.inputEl.placeholder = 'Table of Contents';
+            }).infoEl.classList.add('insta-toc-text-info');
 
+        // Global excluded heading text
         new Setting(containerEl)
-            .setName('Excluded characters')
-            .setDesc('Characters to exclude in headings.')
+            .setName('Excluded heading text')
+            .setDesc('Comma-separated list of headings to exclude globally within the Table of Contents.')
             .addTextArea((component: TextAreaComponent) => {
                 component
-                    .setValue([...new Set(this.plugin.settings.excludedChars)].join(','))
+                    .setValue(this.plugin.settings.excludedHeadingText.join(','))
                     .onChange(async (value: string) => {
                         const textValue = component.getValue();
-                        const excludedChars = new Set([
-                            ...this.plugin.settings.excludedChars,
-                            ...textValue
+                        const excludedHeadingText = [
+                            ...this.plugin.settings.excludedHeadingText
+                        ].concat(
+                            textValue
                                 .replace(/^,/, '').replace(/,$/, '')
                                 .split(',')
                                 .map((value: string) => value.trim())
+                        );
+
+                        this.plugin.settings.excludedHeadingText = [...excludedHeadingText];
+                        await this.plugin.saveSettings();
+                    });
+
+                component.inputEl.placeholder = 'Table of Contents,Introduction,Side Note';
+                component.inputEl.classList.add('insta-toc-text-area');
+            }).infoEl.classList.add('insta-toc-text-info');
+
+        // Global excluded heading levels
+        new Setting(containerEl)
+            .setName('Excluded heading levels')
+            .setDesc('Comma-separated list of heading levels to exclude globally within the Table of Contents.')
+            .setTooltip('Valid values are 1-6.')
+            .addTextArea((component: TextAreaComponent) => {
+                component
+                    .setValue(this.plugin.settings.excludedHeadingLevels.join(','))
+                    .onChange(async (value: string) => {
+                        const textValue = component.getValue();
+                        const excludedHeadingLevels: HeadingLevel[] = textValue
+                            .replace(/^,/, '').replace(/,$/, '')
+                            .split(',')
+                            .map((value: string) => parseInt(value.trim()) as HeadingLevel)
+                            .filter((value: HeadingLevel) => value > 0 && value < 7);
+
+                        this.plugin.settings.excludedHeadingLevels = [...excludedHeadingLevels];
+                        await this.plugin.saveSettings();
+                    });
+
+                component.inputEl.classList.add('insta-toc-text-area');
+                component.inputEl.placeholder = '1,2,3,4,5,6';
+            }).infoEl.classList.add('insta-toc-text-info');
+
+        // Global excluded characters
+        new Setting(containerEl)
+            .setName('Excluded characters')
+            .setDesc('Globally excluded heading characters.')
+            .addTextArea((component: TextAreaComponent) => {
+                component
+                    .setValue([...new Set(this.plugin.settings.excludedChars)].join(','))
+                    .onChange(async (value: string)  => {
+                        const textValue = component.getValue();
+                        const excludedChars = new Set([
+                            ...textValue.trim()
+                                .replace(/^,/, '').replace(/,$/, '')
+                                .split(',')
+                                .map((value: string) => value.trim())
+                                .filter((value: string) => value.length > 0)
                         ]);
 
                         this.plugin.settings.excludedChars = [...excludedChars];
@@ -108,6 +165,7 @@ export class SettingTab extends PluginSettingTab {
                     });
 
                 component.inputEl.classList.add('exclude-chars');
-            });
+                component.inputEl.placeholder = DefaultExcludedChars.join(',');
+            }).infoEl.classList.add('insta-toc-text-info');
     }
 }
