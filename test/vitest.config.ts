6727aa531ac1e94629e80590ecf23f28d1ad9b79
Nick Corbin <nickcorbin17@yahoo.com>
2026-02-25 22:50:01 -0500

    vault backup: 2026-02-25 22:50:01

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -4,12 +4,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
-        environment: 'node',
+        environment: 'jsdom',
         include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-        // transformMode: {
-        //     web: ['\\.[jt]sx$'],
-        // },
-        testNamePattern: '',
         coverage: {
             reporter: ['text', 'json', 'html']
         }
